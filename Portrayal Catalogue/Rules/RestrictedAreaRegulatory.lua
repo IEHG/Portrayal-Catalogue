f8e3e77d2fdbafd4a6c7de58c89112241d5e4aa1
Gert Morlion <85863216+gert-MORLION@users.noreply.github.com>
2024-03-08 13:12:05 +0100

    Update RestrictedAreaRegulatory.lua
    Deleted all alert procedures

--- Portrayal Catalogue/Rules/RestrictedAreaRegulatory.lua
+++ Portrayal Catalogue/Rules/RestrictedAreaRegulatory.lua
@@ -8,14 +8,6 @@ require 'RESARE04'
 function RestrictedAreaRegulatory(feature, featurePortrayal, contextParameters)
 	local viewingGroup = 26020
 
-	if contains(14, feature.restriction) then
-		featurePortrayal:AddInstructions('AlertReference:ProhAre,53015,53015')
-	elseif contains(28, feature.categoryOfRestrictedArea) then
-		featurePortrayal:AddInstructions('AlertReference:ProhAre,53016,53016')
-	elseif not contains(14, feature.restriction) and not contains(28, feature.categoryOfRestrictedArea) then
-		featurePortrayal:AddInstructions('AlertReference:ProhAre,53014,53014')
-	end
-
 	if feature.PrimitiveType == PrimitiveType.Surface and contextParameters.PlainBoundaries then
 		if contains(27, feature.categoryOfRestrictedArea) then
 			featurePortrayal:AddInstructions('ViewingGroup:26020;DrawingPriority:15;DisplayPlane:UnderRADAR')
