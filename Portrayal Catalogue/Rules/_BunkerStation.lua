7389a05f1e616addcfe8fb36e76314fca20121b8
Gert Morlion <85863216+gert-MORLION@users.noreply.github.com>
2024-03-14 11:34:24 +0100

    Add files via upload

---
+++ Portrayal Catalogue/Rules/_BunkerStation.lua
@@ -0,0 +1,48 @@
+-- Converter Version: 0.99
+-- Feature Catalogue Version: 1.1.0 (2024/03/13)
+--
+-- issues to solve: none
+--
+
+-- Anchor bunker station main entry point.
+function BunkerStation(feature, featurePortrayal, contextParameters)
+	local viewingGroup
+
+	if feature.PrimitiveType == PrimitiveType.Point then
+		-- Simplified and paper chart points use the same symbolization
+		viewingGroup = 22410
+		if contextParameters.RadarOverlay then
+			featurePortrayal:AddInstructions('ViewingGroup:22410;DrawingPriority:15;DisplayPlane:OverRADAR')
+		else
+			featurePortrayal:AddInstructions('ViewingGroup:22410;DrawingPriority:15;DisplayPlane:UnderRADAR')
+		end
+        if feature.categoryOfBunkerStation == 1 then
+		    featurePortrayal:AddInstructions('PointInstruction:BUNSTA01')
+        elseif feature.categoryOfBunkerStation == 2 then
+		    featurePortrayal:AddInstructions('PointInstruction:BUNSTA02')
+        elseif feature.categoryOfBunkerStation == 3 then
+		    featurePortrayal:AddInstructions('PointInstruction:BUNSTA03')
+        elseif
+		    featurePortrayal:AddInstructions('PointInstruction:CHINF007')
+        end
+	elseif feature.PrimitiveType == PrimitiveType.Surface then
+		-- Plain and symbolized boundaries use the same symbolization
+		viewingGroup = 22410
+		featurePortrayal:AddInstructions('ViewingGroup:22410;DrawingPriority:15;DisplayPlane:UnderRADAR')
+        if feature.categoryOfBunkerStation == 1 then
+		    featurePortrayal:AddInstructions('PointInstruction:BUNSTA01')
+        elseif feature.categoryOfBunkerStation == 2 then
+		    featurePortrayal:AddInstructions('PointInstruction:BUNSTA02')
+        elseif feature.categoryOfBunkerStation == 3 then
+		    featurePortrayal:AddInstructions('PointInstruction:BUNSTA03')
+        elseif
+		    featurePortrayal:AddInstructions('PointInstruction:CHINF007')
+        end
+		featurePortrayal:SimpleLineStyle('solid',0.32,'CHGRD')
+		featurePortrayal:AddInstructions('LineInstruction:_simple_')
+	else
+		error('Invalid primitive type or mariner settings passed to portrayal')
+	end
+
+	return viewingGroup
+end
---
+++ Portrayal Catalogue/Rules/_IncinerationArea.lua
@@ -0,0 +1,30 @@
+-- Converter Version: 0.99
+-- Feature Catalogue Version: 1.1.0 (2024/03/13)
+--
+-- issues to solve: none
+--
+
+-- Referenced portrayal rules.
+require 'RESTRN01'
+
+-- Anchor IncinerationArea main entry point.
+function IncinerationArea(feature, featurePortrayal, contextParameters)
+	local viewingGroup
+    if feature.PrimitiveType == PrimitiveType.Surface then
+		-- Plain and symbolized boundaries use the same symbolization
+		viewingGroup = 26250
+		if contextParameters.RadarOverlay then
+			featurePortrayal:AddInstructions('ViewingGroup:26250;DrawingPriority:9;DisplayPlane:OverRADAR')
+		else
+			featurePortrayal:AddInstructions('ViewingGroup:26250;DrawingPriority:9;DisplayPlane:UnderRADAR')
+		end
+        featurePortrayal:AddInstructions('PointInstruction:INFRA51')
+        featurePortrayal:SimpleLineStyle('dash',0.30,'TRFCD')
+		featurePortrayal:AddInstructions('LineInstruction:_simple_')
+		RESTRN01(feature, featurePortrayal, contextParameters, viewingGroup)
+	else
+		error('Invalid primitive type or mariner settings passed to portrayal')
+	end
+
+	return viewingGroup
+end
