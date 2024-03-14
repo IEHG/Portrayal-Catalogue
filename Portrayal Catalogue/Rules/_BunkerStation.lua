6638eeb76f6337fb0171c7410c6b282336867c2d
Gert Morlion <85863216+gert-MORLION@users.noreply.github.com>
2024-03-14 11:38:25 +0100

    Update _BunkerStation.lua

--- Portrayal Catalogue/Rules/_BunkerStation.lua
+++ Portrayal Catalogue/Rules/_BunkerStation.lua
@@ -16,28 +16,28 @@ function BunkerStation(feature, featurePortrayal, contextParameters)
 		else
 			featurePortrayal:AddInstructions('ViewingGroup:22410;DrawingPriority:15;DisplayPlane:UnderRADAR')
 		end
-        if feature.categoryOfBunkerStation == 1 then
-		    featurePortrayal:AddInstructions('PointInstruction:BUNSTA01')
-        elseif feature.categoryOfBunkerStation == 2 then
-		    featurePortrayal:AddInstructions('PointInstruction:BUNSTA02')
-        elseif feature.categoryOfBunkerStation == 3 then
-		    featurePortrayal:AddInstructions('PointInstruction:BUNSTA03')
-        elseif
-		    featurePortrayal:AddInstructions('PointInstruction:CHINF007')
-        end
+	        if feature.categoryOfBunkerStation == 1 then
+			    featurePortrayal:AddInstructions('PointInstruction:BUNSTA01')
+	        elseif feature.categoryOfBunkerStation == 2 then
+			    featurePortrayal:AddInstructions('PointInstruction:BUNSTA02')
+	        elseif feature.categoryOfBunkerStation == 3 then
+			    featurePortrayal:AddInstructions('PointInstruction:BUNSTA03')
+	        elseif
+			    featurePortrayal:AddInstructions('PointInstruction:CHINF007')
+	        end
 	elseif feature.PrimitiveType == PrimitiveType.Surface then
 		-- Plain and symbolized boundaries use the same symbolization
 		viewingGroup = 22410
 		featurePortrayal:AddInstructions('ViewingGroup:22410;DrawingPriority:15;DisplayPlane:UnderRADAR')
-        if feature.categoryOfBunkerStation == 1 then
-		    featurePortrayal:AddInstructions('PointInstruction:BUNSTA01')
-        elseif feature.categoryOfBunkerStation == 2 then
-		    featurePortrayal:AddInstructions('PointInstruction:BUNSTA02')
-        elseif feature.categoryOfBunkerStation == 3 then
-		    featurePortrayal:AddInstructions('PointInstruction:BUNSTA03')
-        elseif
-		    featurePortrayal:AddInstructions('PointInstruction:CHINF007')
-        end
+	        if feature.categoryOfBunkerStation == 1 then
+			    featurePortrayal:AddInstructions('PointInstruction:BUNSTA01')
+	        elseif feature.categoryOfBunkerStation == 2 then
+			    featurePortrayal:AddInstructions('PointInstruction:BUNSTA02')
+	        elseif feature.categoryOfBunkerStation == 3 then
+			    featurePortrayal:AddInstructions('PointInstruction:BUNSTA03')
+	        elseif
+			    featurePortrayal:AddInstructions('PointInstruction:CHINF007')
+	        end
 		featurePortrayal:SimpleLineStyle('solid',0.32,'CHGRD')
 		featurePortrayal:AddInstructions('LineInstruction:_simple_')
 	else
