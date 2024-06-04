d3b5dee1fa04d8fa0d426e598896b291703f405f
Gert Morlion <85863216+gert-MORLION@users.noreply.github.com>
2024-06-04 10:42:38 +0200

    Update LandArea.lua
    Amend to S-101 and comment alert reference

--- Portrayal Catalogue/Rules/LandArea.lua
+++ Portrayal Catalogue/Rules/LandArea.lua
@@ -1,5 +1,3 @@
--- Converter Version: 0.99
--- Feature Catalogue Version: 1.0.0 (2019/4/9)
 
 -- Referenced portrayal rules.
 require 'QUAPOS01'
@@ -7,6 +5,13 @@ require 'QUAPOS01'
 -- Land area main entry point.
 function LandArea(feature, featurePortrayal, contextParameters)
 	local viewingGroup
+
+	-- DEBUG tessting #175
+	--feature.featureName[1] = {nameUsage = nil, language = 'fre', name = 'FRE_LAN'}
+	--feature.featureName[2] = {nameUsage = 1, language = 'eng', name = 'ENG_LAN'}
+	--feature.featureName[3] = {nameUsage = nil, language = 'ger', name = 'GER_LAN'}
+
+	-- featurePortrayal:AddInstructions('AlertReference:SafetyContour')
 
 	if feature.PrimitiveType == PrimitiveType.Point then
 		-- Simplified and paper chart points use the same symbolization
@@ -18,7 +23,7 @@ function LandArea(feature, featurePortrayal, contextParameters)
 		end
 		featurePortrayal:AddInstructions('PointInstruction:LNDARE01')
 		if feature.featureName[1] and feature.featureName[1].name then
-			featurePortrayal:AddInstructions('LocalOffset:0,-3.51;TextAlignHorizontal:Center;TextAlignVertical:Center;FontSize:10')
+			featurePortrayal:AddInstructions('LocalOffset:0,-3.51;TextAlignHorizontal:Center;TextAlignVertical:Center;FontSize:10;FontColor:CHBLK')
 			featurePortrayal:AddTextInstruction(EncodeString(GetFeatureName(feature, contextParameters)), 26, 24, 12010, 12)
 		end
 		QUAPOS01(feature, featurePortrayal, contextParameters, viewingGroup)
@@ -31,7 +36,7 @@ function LandArea(feature, featurePortrayal, contextParameters)
 		end
 		QUAPOS01(feature, featurePortrayal, contextParameters, viewingGroup)
 		if feature.featureName[1] and feature.featureName[1].name then
-			featurePortrayal:AddInstructions('LocalOffset:0,-3.51;TextAlignHorizontal:Center;FontSize:10')
+			featurePortrayal:AddInstructions('LocalOffset:0,-3.51;TextAlignHorizontal:Center;FontSize:10;FontColor:CHBLK')
 			featurePortrayal:AddTextInstruction(EncodeString(GetFeatureName(feature, contextParameters)), 26, 24, 12010, 24)
 		end
 	elseif feature.PrimitiveType == PrimitiveType.Surface then
@@ -40,7 +45,7 @@ function LandArea(feature, featurePortrayal, contextParameters)
 		featurePortrayal:AddInstructions('ViewingGroup:12010;DrawingPriority:3;DisplayPlane:UnderRADAR')
 		featurePortrayal:AddInstructions('ColorFill:LANDA')
 		if feature.featureName[1] and feature.featureName[1].name then
-			featurePortrayal:AddInstructions('LocalOffset:-3.51,3.51;TextAlignHorizontal:Center;TextAlignVertical:Center;FontSize:10')
+			featurePortrayal:AddInstructions('LocalOffset:-3.51,3.51;TextAlignHorizontal:Center;TextAlignVertical:Center;FontSize:10;FontColor:CHBLK')
 			featurePortrayal:AddTextInstruction(EncodeString(GetFeatureName(feature, contextParameters)), 26, 24, 12010, 3)
 		end
 	else
