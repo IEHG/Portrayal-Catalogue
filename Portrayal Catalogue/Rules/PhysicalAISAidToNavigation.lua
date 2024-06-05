d3dbaceae4896dcc4c836c01fe47eb53f81bf55d
Gert Morlion <85863216+gert-MORLION@users.noreply.github.com>
2024-06-05 09:49:42 +0200

    Update PhysicalAISAidToNavigation.lua
    If marksNavigationalSystemOf = 11 another approach for AIS AtoN's must be used.

--- Portrayal Catalogue/Rules/PhysicalAISAidToNavigation.lua
+++ Portrayal Catalogue/Rules/PhysicalAISAidToNavigation.lua
@@ -7,16 +7,18 @@ function PhysicalAISAidToNavigation(feature, featurePortrayal, contextParameters
 
 	local viewingGroup = 27240
 
-	if feature.PrimitiveType == PrimitiveType.Point then
-		if contextParameters.RadarOverlay then
-			featurePortrayal:AddInstructions('DisplayPlane:OverRADAR')
+	if feature.marksNavigationalSystemOf ~= 11 then
+		if feature.PrimitiveType == PrimitiveType.Point then
+			if contextParameters.RadarOverlay then
+				featurePortrayal:AddInstructions('DisplayPlane:OverRADAR')
+			else
+				featurePortrayal:AddInstructions('DisplayPlane:UnderRADAR')
+			end
+			featurePortrayal:AddInstructions('AlertReference:NavHazard;ViewingGroup:' .. viewingGroup .. ';DrawingPriority:24;Hover:true;PointInstruction:AISPHY05')
 		else
-			featurePortrayal:AddInstructions('DisplayPlane:UnderRADAR')
+			error('Invalid primitive type or mariner settings passed to portrayal')
 		end
-		featurePortrayal:AddInstructions('AlertReference:NavHazard;ViewingGroup:' .. viewingGroup .. ';DrawingPriority:24;Hover:true;PointInstruction:AISPHY05')
-	else
-		error('Invalid primitive type or mariner settings passed to portrayal')
-	end
+	end 
 
 	return viewingGroup
 end
