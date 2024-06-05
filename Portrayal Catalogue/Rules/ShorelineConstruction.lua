19d57cb2c0fbf13f9dbd973b9804ed9657502029
Gert Morlion <85863216+gert-MORLION@users.noreply.github.com>
2024-06-05 10:38:12 +0200

    Update ShorelineConstruction.lua
    Comment alert instruction

--- Portrayal Catalogue/Rules/ShorelineConstruction.lua
+++ Portrayal Catalogue/Rules/ShorelineConstruction.lua
@@ -8,6 +8,8 @@ require 'SLCONS04'
 function ShorelineConstruction(feature, featurePortrayal, contextParameters)
 	local viewingGroup
 
+	-- featurePortrayal:AddInstructions('AlertReference:SafetyContour')
+
 	if feature.PrimitiveType == PrimitiveType.Point then
 		-- Simplified and paper chart points use the same symbolization
 		viewingGroup = 12410
