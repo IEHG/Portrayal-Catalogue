51e40b441e297b296949c7411016496b24d5e95b
Gert Morlion <85863216+gert-MORLION@users.noreply.github.com>
2024-07-18 12:05:07 +0200

    Add 'beacon' boolean parameter in TOPMAR02

--- Portrayal Catalogue/Rules/SafeWaterBeacon.lua
+++ Portrayal Catalogue/Rules/SafeWaterBeacon.lua
@@ -71,7 +71,7 @@ function SafeWaterBeacon(feature, featurePortrayal, contextParameters)
 			else
 				featurePortrayal:AddInstructions('PointInstruction:BCNGEN03')
 			end
-			TOPMAR02(feature, featurePortrayal, contextParameters, viewingGroup, false)
+			TOPMAR02(feature, featurePortrayal, contextParameters, viewingGroup, false, true)
 			if feature.featureName[1] and feature.featureName[1].name then
 				featurePortrayal:AddInstructions('LocalOffset:-3.51,' .. textOffsetY .. ';TextAlignHorizontal:End;FontSize:10;FontColor:CHBLK')
 				featurePortrayal:AddTextInstruction(EncodeString(GetFeatureName(feature, contextParameters), 'bn %s'), textViewingGroup, textPriority, viewingGroup, priority)
