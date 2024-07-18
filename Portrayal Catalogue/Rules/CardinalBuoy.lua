f5d5d6770fccafbb8658b298d7b39e0920a952d2
Gert Morlion <85863216+gert-MORLION@users.noreply.github.com>
2024-07-18 12:02:49 +0200

    Add 'beacon' boolean parameter in TOPMAR02

--- Portrayal Catalogue/Rules/CardinalBuoy.lua
+++ Portrayal Catalogue/Rules/CardinalBuoy.lua
@@ -79,7 +79,7 @@ function CardinalBuoy(feature, featurePortrayal, contextParameters)
 				featurePortrayal:AddInstructions('PointInstruction:BOYGEN03')
 			end
 			
-			TOPMAR02(feature, featurePortrayal, contextParameters, viewingGroup, true)
+			TOPMAR02(feature, featurePortrayal, contextParameters, viewingGroup, true, false)
 			
 			if feature.featureName[1] and feature.featureName[1].name then
 				featurePortrayal:AddInstructions('LocalOffset:' .. textOffsetX .. ',3.51;TextAlignHorizontal:End;FontSize:10;FontColor:CHBLK')
