0ca9c54fe7950bca38b6a6db9be3038898c8e583
Gert Morlion <85863216+gert-MORLION@users.noreply.github.com>
2024-07-18 12:06:04 +0200

    Add 'beacon' boolean parameter in TOPMAR02

--- Portrayal Catalogue/Rules/SpecialPurposeGeneralBuoy.lua
+++ Portrayal Catalogue/Rules/SpecialPurposeGeneralBuoy.lua
@@ -100,7 +100,7 @@ function SpecialPurposeGeneralBuoy(feature, featurePortrayal, contextParameters)
 			else
 				featurePortrayal:AddInstructions('PointInstruction:BOYGEN03')
 			end
-			TOPMAR02(feature, featurePortrayal, contextParameters, viewingGroup, true)
+			TOPMAR02(feature, featurePortrayal, contextParameters, viewingGroup, true, false)
 			if feature.featureName[1] and feature.featureName[1].name then
 				featurePortrayal:AddInstructions('LocalOffset:' .. textOffsetX .. ',3.51;TextAlignHorizontal:End;FontSize:10;FontColor:CHBLK')
 				featurePortrayal:AddTextInstruction(EncodeString(GetFeatureName(feature, contextParameters), 'by %s'), textViewingGroup, textPriority, viewingGroup, priority)
