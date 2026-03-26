91d0b8644a8b60c718596a48509528b6596821ae
Gert Morlion <85863216+gert-MORLION@users.noreply.github.com>
2026-03-26 16:43:16 +0100

    Add files via upload

---
+++ Portrayal Catalogue/Rules/NoticeMarks.lua
@@ -0,0 +1,16 @@
+-- LUA script
+-- Notice Marks main entry point.
+
+
+ require 'NoticeMark'
+
+function NoticeMarks(feature, featurePortrayal, contextParameters)
+	local viewingGroup
+
+	for mark in feature.NoticeMark
+        -- use position and number of marks to calculate the position in the circle
+
+        -- call the script 'NoticeMark' to display the mark
+    end
+	return viewingGroup
+end
