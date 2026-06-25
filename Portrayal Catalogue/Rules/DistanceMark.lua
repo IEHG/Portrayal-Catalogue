15098f7e4377c017285f902fee79c218d85641d6
Gert Morlion <85863216+gert-MORLION@users.noreply.github.com>
2026-06-25 09:36:32 +0200

    Refactor DistanceMark function for clarity
    Check on complex attribute 'distanceUnitOfMeasurement' has been deleted and replaced by checks on the simple attribute 'distanceUnitOfMeasurement' and 'waterwayDistance'.

--- Portrayal Catalogue/Rules/DistanceMark.lua
+++ Portrayal Catalogue/Rules/DistanceMark.lua
@@ -1,5 +1,4 @@
--- #65
--- #187
+-- #30
 
 -- Distance mark main entry point.
 function DistanceMark(feature, featurePortrayal, contextParameters)
@@ -8,9 +7,9 @@ function DistanceMark(feature, featurePortrayal, contextParameters)
 	if feature.PrimitiveType == PrimitiveType.Point then
 		viewingGroup = 27030
 		if contextParameters.RadarOverlay then
-			featurePortrayal:AddInstructions('ViewingGroup:27030;DrawingPriority:21;DisplayPlane:OverRADAR')
+			featurePortrayal:AddInstructions('ViewingGroup:27030;DrawingPriority:21;DisplayPlane:OverRadar')
 		else
-			featurePortrayal:AddInstructions('ViewingGroup:27030;DrawingPriority:21;DisplayPlane:UnderRADAR')
+			featurePortrayal:AddInstructions('ViewingGroup:27030;DrawingPriority:21;DisplayPlane:UnderRadar')
 		end
 		local textColor = 'CHMGD'
 		local structures = feature:GetFeatureAssociations('StructureEquipment')
@@ -36,41 +35,38 @@ function DistanceMark(feature, featurePortrayal, contextParameters)
 			textColor = 'CHBLK'
 		end
 
-		if feature.measuredDistanceValue
-		then
-			local label = ''
-			
-			local distanceUnitOfMeasurement = feature.measuredDistanceValue.distanceUnitOfMeasurement
-			if distanceUnitOfMeasurement
-			then
-				if distanceUnitOfMeasurement == 1	then
-					label = 'm'
-				elseif distanceUnitOfMeasurement == 2 then
-					label = 'yd'
-				elseif distanceUnitOfMeasurement == 3 then
-					label = 'km'
-				elseif distanceUnitOfMeasurement == 4 then
-					label = 'SM'
-				elseif distanceUnitOfMeasurement == 5 then
-					label = 'M'
-				end
+		local label = ''
+		local distanceUnitOfMeasurement = feature.distanceUnitOfMeasurement
+		if distanceUnitOfMeasurement then
+			if distanceUnitOfMeasurement == 1	then
+				label = 'm'
+			elseif distanceUnitOfMeasurement == 2 then
+				label = 'yd'
+			elseif distanceUnitOfMeasurement == 3 then
+				label = 'km'
+			elseif distanceUnitOfMeasurement == 4 then
+				label = 'SM'
+			elseif distanceUnitOfMeasurement == 5 then
+				label = 'M'
+			elseif distanceUnitOfMeasurement == 7 then
+				label = 'hm'
 			end
-			
-			local waterwayDistance = feature.measuredDistanceValue.waterwayDistance
-			if waterwayDistance
-			then
-				label = label .. tostring(waterwayDistance)
-			end
-			
-			if noStructure then
-				featurePortrayal:AddInstructions('LocalOffset:3.15,0')
-			else
-				featurePortrayal:AddInstructions('LocalOffset:-7.02,0;TextAlignHorizontal:End')
-			end
-		
-			featurePortrayal:AddInstructions('TextAlignVertical:Center;FontWeight:Light;FontColor:' .. textColor)
-			featurePortrayal:AddTextInstruction(EncodeString(label), 21, 24, viewingGroup, 21)
 		end
+			
+		local waterwayDistance = feature.waterwayDistance
+		if waterwayDistance	then
+			label = label .. tostring(waterwayDistance)
+		end
+			
+		if noStructure then
+			featurePortrayal:AddInstructions('LocalOffset:3.15,0')
+		else
+			featurePortrayal:AddInstructions('LocalOffset:-7.02,0;TextAlignHorizontal:End')
+		end
+		
+		featurePortrayal:AddInstructions('TextAlignVertical:Center;FontWeight:Light;FontColor:' .. textColor)
+		featurePortrayal:AddTextInstruction(EncodeString(label), 21, 24, viewingGroup, 21)
+		
 	else
 		error('Invalid primitive type or mariner settings passed to portrayal')
 	end
