863b5bb93a768cb86d2207ca11ce62e4f7acd0bb
Joe Massey <84392130+JoeMassey-WRSystems@users.noreply.github.com>
2026-07-21 09:10:02 -0400

    Code cleanup.

--- Portrayal Catalogue/Rules/MARSYS01.lua
+++ Portrayal Catalogue/Rules/MARSYS01.lua
@@ -1,20 +1,14 @@
 
 function MARSYS01(feature, featurePortrayal, contextParameters, viewingGroup)
-		
 	Debug.StartPerformance('Lua Code - MARSYS01')
 	
-	local marksNavigationalSystemOf = feature.marksNavigationalSystemOf --or 11
-	--                                                                  ^^ uncomment for testing if m_nsys doesn't cover and marksNavigationalSystemOf is missing
-
-	Debug.Break()
+	local marksNavigationalSystemOf = feature.marksNavigationalSystemOf
 
 	if marksNavigationalSystemOf == nil then
 		-- Look for an enclosing NavigationalSystemofMarks feature
 		for _, nsysFeature in ipairs(portrayalContext:GetFeatures('NavigationalSystemOfMarks')) do
-			--Debug.Break()
 			-- if nsysFeature COVERS feature
 			if HostSpatialRelate(nsysFeature.Spatial.SpatialID, feature.Spatial.SpatialID, 'T*****FF*') then
-				--Debug.Break()
 				marksNavigationalSystemOf = nsysFeature.marksNavigationalSystemOf
 				break
 			end
